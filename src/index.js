5a51f6fa04e7a6991a78a41765d03b19482f5388
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-09-14 15:17:24 +0100

    Config setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "./store";
+import App from "./App";
+
+import "./styles/index.css";
+
+ReactDOM.render(
+      <App />
+  document.getElementById("root")
+);
