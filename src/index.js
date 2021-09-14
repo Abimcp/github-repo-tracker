28c41fdf1e0e83ab7e59c272b877f35a2673fe7d
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-09-14 15:18:06 +0100

    config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import store from "./store";
 import App from "./App";
 
-import "./styles/index.css";
-
 ReactDOM.render(
-      <App />
+      <App />,
   document.getElementById("root")
 );
